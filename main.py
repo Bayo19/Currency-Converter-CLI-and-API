c8e846a226be16824358fc2432682fa457bfd893
Bayo <Adebayo.Adegboyega@diageo.com>
2023-01-05 17:54:50 +0000

    finished basic cli

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import typer
 import json
+from rich.console import Console
+from rich.table import Table
 
 app = typer.Typer()
+console = Console()
 
 with open("./rates.json", "r") as rates_json:
     rates = json.load(rates_json)
 
-rates_dict = rates[0]
-for dictionary in rates[1:]:
-    rates_dict.update(dictionary["rates"])
+_rates = {k:v for d in [d["rates"] for d in rates] for k,v in d.items()}
 
+currency_table = Table("Currency Names")
+for key in _rates:
+    currency_table.add_row(key)
 
 @app.command()
 def convert_currency(
     amount: int = typer.Argument(1),
     from_: str = typer.Option(default="USD", help="Currency to convert from"),
     to: str = typer.Option(default="GBP", help="Currency to convert to"),
-):
-    print(amount, from_, to)
+):  
+    if not _rates.get(from_) or _rates.get(to) is None:
+        print("Please use a valid currency name")
+        console.print(currency_table)
+        raise typer.Exit()
 
-# TODO - understand why with one command I don't need to specify the command in the terminal when using the CLI
-# TODO - add logic to get rates from rates_dict and display the currency conversion showing the from and to.
-# TODO - if the rate doesn't exist, show a suitable message and exit 
+    amount_in_new_currency = "{:.2f}".format((amount * _rates.get(to)) / _rates.get(from_))
+    print(f"{amount} {from_} is {amount_in_new_currency} {to}")
+
 
 if __name__ == "__main__":
     app()
