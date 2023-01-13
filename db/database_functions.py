21bbac1b1685767e2957e81af18f08cb3dce5784
Bayo <Bayo.Ade@theaa.com>
2023-01-13 15:53:18 +0000

    Updating type hint for get_rates_from_table

--- db/database_functions.py
+++ db/database_functions.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 from sqlalchemy import or_
 from db.models import Rate
 from db.database import Base, engine, session as _session
@@ -15,7 +15,7 @@ def add_rates_to_table(data: List[CurrencyRate], session=_session) -> None:
     session.commit()
 
 
-def get_rates_from_table(from_rate: str, to_rate: str, session=_session) -> dict:
+def get_rates_from_table(from_rate: str, to_rate: str, session=_session) -> Dict[str, float]:
     result = session.query(Rate).filter(
         or_(Rate.currencyCode == from_rate, Rate.currencyCode == to_rate)
     )
