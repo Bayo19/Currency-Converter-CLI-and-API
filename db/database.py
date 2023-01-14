851d284a90c1ed687188f677bad7fabdd8b87ddd
Bayo <Bayo.Ade@theaa.com>
2023-01-14 08:15:46 +0000

    setting echo to False as user of CLI doesn't need to see the raw SQL behind the commands

--- db/database.py
+++ db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 meta = MetaData()
-engine = create_engine("sqlite:///rates.db", echo=True)
+engine = create_engine("sqlite:///rates.db", echo=False)
 Base = declarative_base()
 # creating session
 Session = sessionmaker(bind=engine)
