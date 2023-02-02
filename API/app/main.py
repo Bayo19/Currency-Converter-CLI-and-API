ef17d3fdd70ac010a10e3527fbd77a9c317d6f3e
Bayo <Bayo.Ade@theaa.com>
2023-02-02 08:28:48 +0000

    Removing comment

--- API/app/main.py
+++ API/app/main.py
@@ -4,13 +4,12 @@ from common import convert
 
 app = FastAPI()
 
-# http://127.0.0.1:8000/convert/?from_currency=USD&to_currency=GBP&amount=400
 @app.get("/convert/")
 def conversion(
     amount: int,
     to_currency: str = Query(default=..., max_length=3),
     from_currency: str = Query(default="USD", max_length=3),
-):  
+):
     converter = convert.FXConverter()
     timestamp = datetime.now()
     conversion_response = dict()
@@ -18,5 +17,5 @@ def conversion(
     conversion_response["conversion"] = converter.convert_currency_(
         amount=amount, convert=from_currency, to=to_currency
     )._asdict()
-    
+
     return conversion_response
