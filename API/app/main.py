661d9952407005bd82ce718c3f0a2197871ac90c
Bayo <Bayo.Ade@theaa.com>
2023-02-02 08:13:55 +0000

    First endpoint added

--- API/app/main.py
+++ API/app/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import FastAPI, Query
 from common import convert
 
@@ -9,8 +10,13 @@ def conversion(
     amount: int,
     to_currency: str = Query(default=..., max_length=3),
     from_currency: str = Query(default="USD", max_length=3),
-):
+):  
     converter = convert.FXConverter()
-    return converter.convert_currency_(
+    timestamp = datetime.now()
+    conversion_response = dict()
+    conversion_response["timestamp"] = timestamp
+    conversion_response["conversion"] = converter.convert_currency_(
         amount=amount, convert=from_currency, to=to_currency
-    )
+    )._asdict()
+    
+    return conversion_response
