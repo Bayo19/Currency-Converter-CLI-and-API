f125cbaa8bf76a90effe0f05badd906684862908
Bayo <Bayo.Ade@theaa.com>
2023-02-10 10:24:09 +0000

    creating new models

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Float, Column
+from sqlalchemy import String, Float, Column, Integer, ForeignKey, UniqueConstraint, DateTime
 from db.database import Base
 
 # Model
@@ -7,3 +7,19 @@ class Rate(Base):
     __tablename__ = "exchangerates"
     currency_code = Column(String, primary_key=True)
     rate = Column(Float)
+
+
+class Portfolio(Base):
+    timestamp = Column()
+    __tablename__ = "portfolio"
+    username = Column(String)
+    id = Column(Integer, primary_key=True)
+    __table_args__ = UniqueConstraint('username')
+
+
+class PortfolioBalance(Base):
+    __tablename__ = "portfolio_balance"
+    portfolio_id = Column(Integer, ForeignKey("portfolio.id"))
+    currency = Column(String)
+    amount = Column(Float)
+    timestamp = Column(DateTime)
