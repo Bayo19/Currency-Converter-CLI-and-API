20a9e94c2e7527f7fd2f4b035e02cc8233fc7898
Bayo <Bayo.Ade@theaa.com>
2023-02-25 17:18:52 +0000

    Updating dataclass with decorator and adding PortfolioItem dataclass

--- common/data_types.py
+++ common/data_types.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+from datetime import datetime
 
 @dataclass
 class CurrencyConversion:
@@ -15,6 +15,15 @@ class CountryCode:
     country_code: str
 
 
+@dataclass
 class CurrencyRate:
     code: str
     rate: float
+
+@dataclass
+class PortfolioItem:
+    username: str
+    user_id: int
+    currency_code: str
+    amount: str
+    timestamp: datetime
