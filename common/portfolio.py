22e91a155955883483d01bb402265f58ebb3d54c
Bayo <Bayo.Ade@theaa.com>
2023-03-07 16:52:55 +0000

    Refactoring portfolio class. Creating and using custom exception

--- common/portfolio.py
+++ common/portfolio.py
@@ -1,43 +1,42 @@
 from __future__ import annotations
-from typing import Optional, Any
+from typing import Any
 from functools import reduce
 from db.database_functions import (
     add_balances_to_portfolio_balance_table,
     create_new_portfolio_user,
     user_exists,
     get_portfolio_from_table,
-    subtract_amount_from_currency,
-    add_amount_to_currency,
 )
-
-import common.convert as convert
+from common.convert import FXConverter
+from common.utility_functions import valid_trade, trade
+from common.custom_exceptions import UserNotFoundError
 
 
 class Portfolio:
     def __init__(self, username: str) -> None:
         self.username = username
-        self.converter = convert.FXConverter()
+        self.converter = FXConverter()
 
-    def create_portfolio(self, balance_map: dict[str, float]) -> bool:
+    def create_portfolio(self, currency_balances: dict[str, float]) -> bool:
         """
         Create new portfolio user in portfolio table and add currencies to portfolio_balance table
         """
         new_user = create_new_portfolio_user(username=self.username)
-        if new_user:
-            add_balances_to_portfolio_balance_table(
-                username=self.username, balance_map=balance_map
-            )
-            return True
-        return False
+        if not new_user:
+            return False
+        add_balances_to_portfolio_balance_table(
+            username=self.username, currency_balances=currency_balances
+        )
+        return True
 
-    def update_portfolio_balance(self, balance_map: dict[str, float]) -> None:
+    def update_portfolio_balance(self, currency_balances: dict[str, float]) -> None:
         """
         Update currencies in portflio_balance for user (username)
         """
         if not user_exists(username=self.username):
-            raise ValueError
+            raise UserNotFoundError
         add_balances_to_portfolio_balance_table(
-            username=self.username, balance_map=balance_map
+            username=self.username, currency_balances=currency_balances
         )
 
     def get_portfolio_as_dict(
@@ -68,8 +67,8 @@ class Portfolio:
                 lambda accumulator, current: accumulator
                 + self.converter.convert_currency_(
                     amount=current.amount,
-                    convert=current.currency_code,
-                    to=base_currency,
+                    source_currency=current.currency_code,
+                    target_currency=base_currency,
                 ).converted_amount,
                 portfolio_items,
                 0,
@@ -79,63 +78,41 @@ class Portfolio:
 
     def trade_currencies(
         self,
-        source_currency_code: str,
-        target_currency_code: str,
-        source_amount: str,
-        other_portfolio_name: str,
+        buyer_currency_code: str,
+        seller_currency_code: str,
+        buyer_amount: str,
+        seller_portfolio_name: str,
     ) -> bool:
 
         source_portfolio = get_portfolio_from_table(username=self.username)
-        target_portfolio = get_portfolio_from_table(username=other_portfolio_name)
+        target_portfolio = get_portfolio_from_table(username=seller_portfolio_name)
 
         if not source_portfolio or not target_portfolio:
             raise ValueError
 
-        target_amount = float(
+        seller_amount = float(
             self.converter.convert_currency_(
-                amount=source_amount,
-                convert=source_currency_code,
-                to=target_currency_code,
+                amount=buyer_amount,
+                source_currency=buyer_currency_code,
+                target_currency=seller_currency_code,
             ).converted_amount
         )
 
-        other_user_has_target_currency = bool(
-            [
-                t
-                for t in target_portfolio
-                if t.currency_code == target_currency_code and t.amount >= target_amount
-            ]
-        )
-
-        source_user_has_source_currency = bool(
-            [
-                s
-                for s in source_portfolio
-                if s.currency_code == source_currency_code and s.amount >= source_amount
-            ]
-        )
-
-        if source_user_has_source_currency and other_user_has_target_currency:
-            subtract_amount_from_currency(
-                username=self.username,
-                currency=source_currency_code,
-                amount=source_amount,
-            )
-            add_amount_to_currency(
-                username=other_portfolio_name,
-                currency=source_currency_code,
-                amount=source_amount,
-            )
-
-            subtract_amount_from_currency(
-                username=other_portfolio_name,
-                currency=target_currency_code,
-                amount=target_amount,
-            )
-            add_amount_to_currency(
-                username=self.username,
-                currency=target_currency_code,
-                amount=target_amount,
+        if valid_trade(
+            buyer_portfolio=source_portfolio,
+            seller_portfolio=target_portfolio,
+            buyer_currency_code=buyer_currency_code,
+            seller_currency_code=seller_currency_code,
+            buyer_amount=buyer_amount,
+            seller_amount=seller_amount,
+        ):
+            trade(
+                buyer_currency_code=buyer_currency_code,
+                seller_currency_code=seller_currency_code,
+                buyer_amount=buyer_amount,
+                seller_amount=seller_amount,
+                buyer_username=self.username,
+                seller_username=seller_portfolio_name,
             )
             return True
         else:
