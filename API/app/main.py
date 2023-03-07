9eb2fe174d4dce34f7f754e50df537a337147b92
Bayo <Bayo.Ade@theaa.com>
2023-03-07 16:48:25 +0000

    Abstracting away some logic

--- API/app/main.py
+++ API/app/main.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 from typing import Any
-from fastapi import FastAPI, Query, status, HTTPException
-from common import convert, portfolio
+from fastapi import FastAPI, Query, status, HTTPException, Depends
+from common.convert import FXConverter
+from common.portfolio import Portfolio
 from db.schemas import UserPortfolio, ConversionData, CreatePortfolio
 
 app = FastAPI()
+
+
+def user_portfolio_instance(username: str) -> Portfolio:
+    return Portfolio(username=username)
 
 
 @app.get("/convert/", status_code=status.HTTP_200_OK)
@@ -13,9 +18,9 @@ def conversion(
     to_currency: str = Query(default=..., max_length=3),
     from_currency: str = Query(default="USD", max_length=3),
 ) -> ConversionData:
-    converter = convert.FXConverter()
+    converter = FXConverter()
     conversion_result = converter.convert_currency_(
-        amount=amount, convert=from_currency, to=to_currency
+        amount=amount, source_currency=from_currency, target_currency=to_currency
     )
     if conversion_result:
         timestamp = datetime.now()
@@ -32,19 +37,22 @@ def conversion(
 
 @app.post("/create_portfolio/", status_code=status.HTTP_201_CREATED)
 def initialise_portfolio(request: CreatePortfolio) -> dict[str, Any]:
-    portf = portfolio.Portfolio(username=request.username)
-    if portf.create_portfolio(balance_map=request.balance_map):
-        return {"success": "true", "message": "Portfolio created", "x": request}
+    user_portfolio = Portfolio(
+        username=request.username
+    )  # not sure if I can use the dependency here
+    if user_portfolio.create_portfolio(currency_balances=request.currency_balances):
+        return {"success": "true", "message": "Portfolio created", "data": request}
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"User: {request.username}, already exists.",
     )
 
 
-@app.get("/portfolio/{username}", status_code=status.HTTP_200_OK)
-def get_portfolio(username: str) -> UserPortfolio:
-    portf = portfolio.Portfolio(username=username)
-    portfolio_dict_result = portf.get_portfolio_as_dict()
+@app.get("/portfolio/", status_code=status.HTTP_200_OK)
+def get_portfolio(
+    username: str, user_portfolio: Portfolio = Depends(user_portfolio_instance)
+) -> UserPortfolio:
+    portfolio_dict_result = user_portfolio.get_portfolio_as_dict()
     if portfolio_dict_result:
         return portfolio_dict_result
     raise HTTPException(
@@ -52,22 +60,23 @@ def get_portfolio(username: str) -> UserPortfolio:
         detail=f"Username: {username} - does not exist",
     )
 
-
-@app.put("/trade/{buyer_username}", status_code=status.HTTP_200_OK)
+# make more idempotent by recording each trade and 
+# not allowing same trade to take place more than once?
+@app.post("/trade/", status_code=status.HTTP_200_OK)
 def make_trade(
-    buyer_username: str,
+    username: str,
     source_amount: int,
     seller_username: str,
     source_currency: str = Query(default=..., max_length=3),
     target_currency: str = Query(default=..., max_length=3),
+    user_portfolio: Portfolio = Depends(user_portfolio_instance),
 ) -> dict[str, Any]:
-    portf = portfolio.Portfolio(username=buyer_username)
 
-    currency_trade = portf.trade_currencies(
-        source_currency_code=source_currency,
-        target_currency_code=target_currency,
-        source_amount=source_amount,
-        other_portfolio_name=seller_username,
+    currency_trade = user_portfolio.trade_currencies(
+        buyer_currency_code=source_currency,
+        seller_currency_code=target_currency,
+        buyer_amount=source_amount,
+        seller_portfolio_name=seller_username,
     )
     try:
         if currency_trade:
@@ -78,7 +87,7 @@ def make_trade(
                 "success": "true",
                 "message": "Trade successful",
                 "details": {
-                    "buyer": buyer_username,
+                    "buyer": username,
                     "seller": seller_username,
                     "currency": target_currency,
                     "amount": source_amount,
---
+++ common/utility_functions.py
@@ -0,0 +1,95 @@
+import os
+from typing import List
+import requests
+import common.api_keys
+from common.country_codes_mapping import country_currency_codes
+from common.data_types import CountryCode, CurrencyRate, PortfolioItem
+from db.database_functions import subtract_amount_from_currency, add_amount_to_currency
+
+
+def ingest_rates() -> List[CurrencyRate]:
+    """
+    Downloads the latest exchange rates from openexchangerates.org and writes them to rates.json
+    to make them available for conversion queries
+    """
+    api_key = os.environ["API_KEY"]
+    url = f"https://openexchangerates.org/api/latest.json?app_id={api_key}"
+    response = requests.get(url, headers={"accept": "application/json"})
+
+    rates = [CurrencyRate(code=k, rate=v) for k, v in response.json()["rates"].items()]
+    return rates
+
+
+def get_country_code(country: str) -> CountryCode:
+    """
+    Returns a country code
+    """
+
+    filtered_codes = {
+        k: v for k, v in country_currency_codes.items() if country.upper() in k
+    }
+    try:
+        code = list(v for v in filtered_codes.values())[0]
+    except IndexError:
+        return None
+    # the key for the code doesn't always match the param: country
+    return CountryCode(country=country.upper(), country_code=code)
+
+
+def valid_trade(
+    buyer_portfolio: list[PortfolioItem],
+    seller_portfolio: list[PortfolioItem],
+    buyer_currency_code,
+    seller_currency_code: str,
+    buyer_amount: float,
+    seller_amount: float,
+) -> bool:
+
+    seller_has_enough_money = bool(
+        [
+            t
+            for t in seller_portfolio
+            if t.currency_code == seller_currency_code and t.amount >= seller_amount
+        ]
+    )
+
+    buyer_has_enough_money = bool(
+        [
+            s
+            for s in buyer_portfolio
+            if s.currency_code == buyer_currency_code and s.amount >= buyer_amount
+        ]
+    )
+
+    return bool(seller_has_enough_money and buyer_has_enough_money)
+
+
+def trade(
+    buyer_currency_code: str,
+    seller_currency_code: str,
+    buyer_amount: float,
+    seller_amount: float,
+    buyer_username: str,
+    seller_username,
+) -> None:
+    subtract_amount_from_currency(
+        username=buyer_username,
+        currency=buyer_currency_code,
+        amount=buyer_amount,
+    )
+    add_amount_to_currency(
+        username=seller_username,
+        currency=buyer_currency_code,
+        amount=buyer_amount,
+    )
+
+    subtract_amount_from_currency(
+        username=seller_username,
+        currency=seller_currency_code,
+        amount=seller_amount,
+    )
+    add_amount_to_currency(
+        username=buyer_username,
+        currency=seller_currency_code,
+        amount=seller_amount,
+    )
