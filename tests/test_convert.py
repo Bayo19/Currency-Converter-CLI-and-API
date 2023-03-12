eedddf6f801aecd5456115659b35d94bca73f450
Bayo <Bayo.Ade@theaa.com>
2023-03-12 07:18:28 +0000

    Updating test for convert_currency so test can use mock rates

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -15,7 +15,7 @@ from unittest.mock import patch
                 source_currency_code="USD",
                 target_currency_code="GBP",
                 requested_amount=500,
-                converted_amount=421.02250000000004,
+                converted_amount=415.4895,
             ),
         ),
         (
@@ -26,7 +26,7 @@ from unittest.mock import patch
                 source_currency_code="JPY",
                 target_currency_code="GBP",
                 requested_amount=69_500,
-                converted_amount=427.8624507321724,
+                converted_amount=427.81633122863406,
             ),
         ),
         (
@@ -37,7 +37,7 @@ from unittest.mock import patch
                 source_currency_code="GBP",
                 target_currency_code="USD",
                 requested_amount=1000,
-                converted_amount=1187.5849865506,
+                converted_amount=1203.39984524278,
             ),
         ),
         (
@@ -48,23 +48,21 @@ from unittest.mock import patch
                 source_currency_code="GBP",
                 target_currency_code="JPY",
                 requested_amount=50,
-                converted_amount=8121.7690265959645,
+                converted_amount=8122.644570440409,
             ),
         ),
-        (135, "USD", "ZZZ", None),
-        (200_069_420, "ZZZ", "USD", None),
     ],
 )
 def test_convert_currency(
     amount: float, source_currency_code: str, target_currency_code: str, expected
 ):
     def mock_get_rates_func(from_rate, to_rate):
-        rates_dict = {"GBP": 0.806753, "JPY": 128.127, "EUR": 0.920924}
-        if from_rate not in rates_dict or to_rate not in rates_dict:
+        rates_dict = {"GBP": 0.806753, "JPY": 128.127, "EUR": 0.920924, "USD": 1.234}
+        if from_rate not in rates_dict.keys() or to_rate not in rates_dict.keys():
             raise ValueError
-        return {k: v for k, v in rates_dict if k == from_rate or k == to_rate}
+        return {k: v for k, v in rates_dict.items() if k == from_rate or k == to_rate}
 
-    p = patch("common.convert.get_rates_from_table", new=mock_get_rates_func)
+    patch("common.convert.get_rates_from_table", new=mock_get_rates_func(source_currency_code, target_currency_code))
     converter = FXConverter()
     result = converter.convert_currency_(
         amount=amount,
